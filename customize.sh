af53ca27a69c31e27835624d0875f674a6c10387
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2020-07-21 22:17:33 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# Description: Build OpenWrt using GitHub Actions
+# Lisence: MIT
+# Author: AndroidDeals
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
