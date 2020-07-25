60ab2b9a3c28c52345257ead2d8b73cd351e1c44
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2020-07-25 21:11:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Deassociate bootstrap as default theme
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' openwrt/feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
