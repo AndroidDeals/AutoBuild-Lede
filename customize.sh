19c1a29df9ea9261103bba629c619284592e3864
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2021-12-14 23:33:54 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,3 +12,6 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' openwrt/feeds/lu
 
 #3. Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
+echo 'src-git maxminddb https://github.com/jerrykuku/lua-maxminddb.git' >>feeds.conf.default
+echo 'src-git https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
