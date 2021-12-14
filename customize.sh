801bddbe0524b3553cc39ce45446c9500fb5ffc3
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2021-12-14 23:54:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,8 +10,3 @@ sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/confi
 #2. Deassociate bootstrap as default theme
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' openwrt/feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-#3. Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
-echo 'src-git maxminddb https://github.com/jerrykuku/lua-maxminddb.git' >>feeds.conf.default
-echo 'src-git https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
