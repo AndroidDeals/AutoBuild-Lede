0da8d41e17a14b1eb37300c7fb4ed1ecd89972ad
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2021-12-16 11:56:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,7 +5,7 @@
 # Author: AndroidDeals
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Deassociate bootstrap as default theme
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' openwrt/feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
